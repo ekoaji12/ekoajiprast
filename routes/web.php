b8658a58d7a6be559521c928c49d272ca68dd793
ekoajiprast <ekoajiprasetyo300600@gmail.com>
2022-07-05 08:41:56 +0700

    laravel8

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/route1', function () {
+    echo "<h1> Ini Adalah Halaman Test 1 <h1>";
+});
+Route::get('/route2', function () {
+    echo "<h1> Ini Adalah Halaman Test 2 <h1>";
+});
